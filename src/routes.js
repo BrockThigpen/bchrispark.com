04359edd4e4e8cf5258cf86f8b9c8f28b51ea29f
Brock T <brockthigpen@yahoo.com>
2020-10-29 00:01:12 -0400

    trouble shooting

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
-import react from 'react';
+import React from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
   } from "react-router-dom";
 
   export default function App() {
