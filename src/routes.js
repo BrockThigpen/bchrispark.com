8c5d9eaa9aee31c91d1dc25cffd3be88917ecc75
Brock T <brockthigpen@yahoo.com>
2020-11-01 23:10:29 -0500

    memoizing

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import {
     BrowserRouter as Router,
     Switch,
@@ -6,26 +6,38 @@ import {
   } from "react-router-dom";
 import Resume from './pages/Resume';
 import SwapPagesButton from './components/SwapPages'
+import RenderHugeList from './components/RenderHugeList'
 
 export const PATHS = {
     home: '/',
-    resume: '/resume'
+    resume: '/resume',
+    memoizeThis: '/memoizeThis'
 }
 
 export default function App() {
+    const [changeThis, setChangeThis] = useState('')
+
+    const anotherFunction = useCallback(() => {
+        console.log('hello')
+    }, [])
+
       return (
-          <Router>
-              <Switch>
-                  <Route exact path={PATHS.home}>
-                      <div>Home</div> 
-                      <SwapPagesButton />
-                  </Route>
+        <Router>
+            <Switch>
+                <Route exact path={PATHS.home}>
+                    <div>Home</div> 
+                    <SwapPagesButton />
+                </Route>
 
-                  <Route path={PATHS.resume}>
-                      <Resume/>
-                  </Route>
+                <Route path={PATHS.resume}>
+                    <div>Resume</div>
+                    <Resume/>
+                </Route>
 
-              </Switch>
-          </Router>
+                <Route path={PATHS.memoizeThis}>
+                <RenderHugeList anotherFunction={anotherFunction} changeThis={changeThis} setChangeThis={setChangeThis} />
+                </Route>
+            </Switch>
+        </Router>
       )
   }
