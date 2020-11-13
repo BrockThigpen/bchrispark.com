7638203e85536e3ba31a33e04357f9bf9d592c51
Brock T <brockthigpen@yahoo.com>
2020-11-12 22:00:45 -0500

    removed momozie example added home page and portfolio page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,19 @@
+import SwapPagesButton from '../../components/SwapPages';
+
+export default function HomePage () {
+    const style = {
+        display: 'flex', 
+        width: '100vw', 
+        height: '100vh', 
+        justifyContent: 'center', 
+        alignItems: "center"
+    }
+    return(
+        <>
+           <SwapPagesButton />
+            <div style={style}>
+                <img alt={'fat arthur'} src={'https://bit.ly/3kLsVGg'}/>
+            </div>
+        </>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -5,37 +5,30 @@ import {
     Route,
   } from "react-router-dom";
 import Resume from './pages/Resume';
-import SwapPagesButton from './components/SwapPages'
-import RenderHugeList from './components/RenderHugeList'
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+
 
 export const PATHS = {
     home: '/',
     resume: '/resume',
-    memoizeThis: '/memoizeThis'
+    portfolio: '/portfolio'
 }
 
 export default function App() {
-    const [changeThis, setChangeThis] = useState('')
-
-    const anotherFunction = useCallback(() => {
-        console.log('hello')
-    }, [])
-
       return (
         <Router>
             <Switch>
-                <Route exact path={PATHS.home}>
-                    <div>Home</div> 
-                    <SwapPagesButton />
+            <Route exact path={PATHS.home}>
+                    <Home/>
                 </Route>
 
                 <Route path={PATHS.resume}>
-                    <div>Resume</div>
-                    <Resume/>
+                    <Resume />
                 </Route>
 
-                <Route path={PATHS.memoizeThis}>
-                <RenderHugeList anotherFunction={anotherFunction} changeThis={changeThis} setChangeThis={setChangeThis} />
+                <Route path={PATHS.portfolio}>
+                    <Portfolio />
                 </Route>
             </Switch>
         </Router>
